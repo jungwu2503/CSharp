21f89694e60c1407e6151ada58470dbd1ef3d8d9
술든라이언 <85924877+jungwu2503@users.noreply.github.com>
2022-02-13 23:15:18 +0900

    Add files via upload
    즉석 복권 스타일의 프로그램

---
+++ InstantLotto/InstantLotto/InstantLotto.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace InstantLotto
+{
+    partial class InstantLotto
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.canvas = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // canvas
+            // 
+            this.canvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.canvas.Location = new System.Drawing.Point(0, 0);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(475, 347);
+            this.canvas.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.canvas.TabIndex = 0;
+            this.canvas.TabStop = false;
+            this.canvas.Click += new System.EventHandler(this.canvas_Click);
+            this.canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.canvas_Paint);
+            this.canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseDown);
+            this.canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseMove);
+            this.canvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseUp);
+            // 
+            // InstantLotto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 347);
+            this.Controls.Add(this.canvas);
+            this.Name = "InstantLotto";
+            this.Text = "로또~";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox canvas;
+    }
+}
+
---
+++ InstantLotto/InstantLotto/InstantLotto.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace InstantLotto
+{
+    public partial class InstantLotto : Form
+    {
+        Bitmap backImage;
+        bool dragging;
+        Bitmap canvasShot;
+
+        public InstantLotto()
+        {
+            InitializeComponent();
+            dragging = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            backImage = new Bitmap("instantLotto.gif");
+            //canvas.Image = backImage;
+            SetBounds(0, 0, backImage.Width+10, backImage.Height+30);
+        }
+
+        private void canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (dragging == false) return;
+
+            Graphics g = canvas.CreateGraphics();
+
+            g.CompositingMode = CompositingMode.SourceOver;
+            TextureBrush brush = new TextureBrush(backImage);
+
+            g.FillEllipse(brush, e.X, e.Y, 20, 20);
+
+            brush.Dispose();
+            g.Dispose();
+        }
+
+        private void canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            dragging = true;
+        }
+
+        [System.Runtime.InteropServices.DllImportAttribute("gdi32.dll")]
+        private static extern bool BitBlt(
+            IntPtr a, int b, int c, int d, int e, IntPtr f, int g, int h, System.Int32 i);
+
+        private void canvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            dragging = false;
+
+            Rectangle r = canvas.ClientRectangle;
+            canvasShot = new Bitmap(r.Width, r.Height);
+
+            Graphics g1 = canvas.CreateGraphics();
+            Graphics g2 = Graphics.FromImage(canvasShot);
+
+            IntPtr dc1 = g1.GetHdc();
+            IntPtr dc2 = g2.GetHdc();
+
+            BitBlt(dc2, 0, 0, r.Width, r.Height, dc1, 0, 0, 0x00CC0020);
+
+            g1.ReleaseHdc(dc1);
+            g2.ReleaseHdc(dc2);
+            g1.Dispose();
+            g2.Dispose();
+        }
+
+        private void canvas_Paint(object sender, PaintEventArgs e)
+        {            
+            if (canvasShot == null) return;
+            e.Graphics.DrawImage(canvasShot, 0, 0);
+            //canvas.Image = canvasShot;
+        }
+
+        private void canvas_Click(object sender, EventArgs e)
+        {
+            Graphics g = canvas.CreateGraphics();
+            Pen pen = new Pen(Color.Black);
+            g.DrawLine(pen, backImage.Width / 2 - 7, 0, backImage.Width / 2 - 7, backImage.Height);
+            g.DrawLine(pen, 0, backImage.Height/4 - 7, backImage.Width, backImage.Height/4 - 7);
+            g.DrawLine(pen, 0, backImage.Height / 2 - 12, backImage.Width, backImage.Height / 2 - 12);
+            g.DrawLine(pen, 0, backImage.Height / 4 * 3 - 5, backImage.Width, backImage.Height / 4 * 3 - 5);
+            
+            pen.Dispose();
+            g.Dispose();
+        }
+
+    }
+}
