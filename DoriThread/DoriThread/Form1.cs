8868f7cad25d08f1388238c38ec6ff3fde4b7a96
술든라이언 <85924877+jungwu2503@users.noreply.github.com>
2022-02-18 18:28:44 +0900

    Add files via upload

--- DoriThread/DoriThread/Form1.cs
+++ DoriThread/DoriThread/Form1.cs
@@ -12,15 +12,60 @@ namespace DoriThread
 {
     public partial class Form1 : Form
     {
+        private enum enumPlayer
+        {
+            손흥민,
+            김덕배,
+            최앙리,
+            박무리뉴,
+            오메시,
+        }
+
+        int _locationX = 0;
+        int _locationY = 0;
+
         public Form1()
         {
             InitializeComponent();
+
+            _locationX = Location.X;
+            _locationY = Location.Y;
         }
 
         private void btnStart_Click(object sender, EventArgs e)
         {
-            Play pl = new Play();
-            pl.Show();
+            _locationX = Location.X + Size.Width;
+            _locationY = Location.Y;
+
+            for (int i = 0; i < numPlayerCount.Value; i++)
+            {
+                Play pl = new Play(((enumPlayer)i).ToString());
+                pl.Location = new Point(_locationX, _locationY + pl.Height * i);
+
+                pl.eventdelMessage += pl_eventdelMessage;
+
+                pl.Show();
+                pl.fThreadStart();
+            }
+
+            
         }
+
+        int pl_eventdelMessage(object sender, string strResult)
+        {
+
+            if (InvokeRequired) // 요청한 Thread가 현재 Main Thread에 있는 Control을 엑세스할수 있는지 확인
+            {
+                Invoke(new Action(delegate()
+                {
+
+                    Play oPlayerForm = sender as Play;
+
+                    lboxResult.Items.Add(string.Format("Player : {0}, Text : {1}", oPlayerForm.StrPlayerName, strResult));
+                }));
+            }
+            return 0;
+        }
+
     }
 }
