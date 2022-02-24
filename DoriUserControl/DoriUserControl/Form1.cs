85d0285947554a3ca760f676a21443e16488f1c8
술든라이언 <85924877+jungwu2503@users.noreply.github.com>
2022-02-24 16:49:53 +0900

    Add files via upload

---
+++ DoriUserControl/DoriUserControl/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoriUserControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (var oControl in pMain.Controls)
+            {
+                if (oControl is UCInfo)
+                {
+                    UCInfo oInfo = oControl as UCInfo;
+
+                    oInfo.eventdelSender += oInfo_eventdelSender;
+                }
+            }
+        }
+
+        int oInfo_eventdelSender(object sender, string strText)
+        {
+            UCInfo oInfo = sender as UCInfo;
+
+            lboxList.Items.Add(string.Format("{0}) {1}", oInfo.UserNo, strText));
+
+            return 0;
+        }
+
+
+       
+    }
+}
