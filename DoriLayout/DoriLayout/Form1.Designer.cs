075d3347d373db2987987bba59c5547aa41ceeda
술든라이언 <85924877+jungwu2503@users.noreply.github.com>
2022-03-04 08:52:05 +0900

    Add files via upload

--- DoriLayout/DoriLayout/Form1.Designer.cs
+++ DoriLayout/DoriLayout/Form1.Designer.cs
@@ -31,11 +31,12 @@
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-            this.ucPanel1 = new DoriLayout.ucPanel();
-            this.ucPanel3 = new DoriLayout.ucPanel();
-            this.ucPanel4 = new DoriLayout.ucPanel();
-            this.ucPanel5 = new DoriLayout.ucPanel();
-            this.ucPanel2 = new DoriLayout.ucPanel();
+            this.ucCMenu = new DoriLayout.ucColorMenu();
+            this.ucPanelCenter1 = new DoriLayout.ucPanel();
+            this.ucPanelCenter2 = new DoriLayout.ucPanel();
+            this.ucPanelRight = new DoriLayout.ucPanel();
+            this.ucPanelTop = new DoriLayout.ucPanel();
+            this.lboxLog = new System.Windows.Forms.ListBox();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -54,13 +55,13 @@
             // splitContainer1.Panel1
             // 
             this.splitContainer1.Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
-            this.splitContainer1.Panel1.Controls.Add(this.ucPanel1);
+            this.splitContainer1.Panel1.Controls.Add(this.ucCMenu);
             // 
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.tableLayoutPanel1);
-            this.splitContainer1.Size = new System.Drawing.Size(508, 395);
-            this.splitContainer1.SplitterDistance = 169;
+            this.splitContainer1.Size = new System.Drawing.Size(508, 444);
+            this.splitContainer1.SplitterDistance = 120;
             this.splitContainer1.SplitterWidth = 2;
             this.splitContainer1.TabIndex = 0;
             // 
@@ -68,16 +69,18 @@
             // 
             this.tableLayoutPanel1.BackColor = System.Drawing.Color.Lime;
             this.tableLayoutPanel1.ColumnCount = 1;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.ucPanel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.ucPanelTop, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lboxLog, 0, 2);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowCount = 3;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 26.58228F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 73.41772F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(337, 395);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(386, 444);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // tableLayoutPanel2
@@ -85,76 +88,87 @@
             this.tableLayoutPanel2.ColumnCount = 2;
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.Controls.Add(this.ucPanel3, 0, 0);
-            this.tableLayoutPanel2.Controls.Add(this.ucPanel4, 0, 1);
-            this.tableLayoutPanel2.Controls.Add(this.ucPanel5, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.ucPanelCenter1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.ucPanelCenter2, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.ucPanelRight, 1, 0);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 108);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 99);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 2;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(331, 284);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(380, 261);
             this.tableLayoutPanel2.TabIndex = 0;
             // 
-            // ucPanel1
+            // ucCMenu
             // 
-            this.ucPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.ucPanel1.Location = new System.Drawing.Point(0, 0);
-            this.ucPanel1.Margin = new System.Windows.Forms.Padding(0);
-            this.ucPanel1.Name = "ucPanel1";
-            this.ucPanel1.Size = new System.Drawing.Size(169, 395);
-            this.ucPanel1.TabIndex = 0;
+            this.ucCMenu.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ucCMenu.Location = new System.Drawing.Point(0, 0);
+            this.ucCMenu.Name = "ucCMenu";
+            this.ucCMenu.Size = new System.Drawing.Size(120, 444);
+            this.ucCMenu.TabIndex = 0;
             // 
-            // ucPanel3
+            // ucPanelCenter1
             // 
-            this.ucPanel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.ucPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.ucPanel3.Location = new System.Drawing.Point(0, 0);
-            this.ucPanel3.Margin = new System.Windows.Forms.Padding(0);
-            this.ucPanel3.Name = "ucPanel3";
-            this.ucPanel3.Size = new System.Drawing.Size(165, 142);
-            this.ucPanel3.TabIndex = 0;
+            this.ucPanelCenter1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ucPanelCenter1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ucPanelCenter1.Location = new System.Drawing.Point(0, 0);
+            this.ucPanelCenter1.Margin = new System.Windows.Forms.Padding(0);
+            this.ucPanelCenter1.Name = "ucPanelCenter1";
+            this.ucPanelCenter1.Size = new System.Drawing.Size(190, 130);
+            this.ucPanelCenter1.TabIndex = 0;
             // 
-            // ucPanel4
+            // ucPanelCenter2
             // 
-            this.ucPanel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            this.ucPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.ucPanel4.Location = new System.Drawing.Point(0, 142);
-            this.ucPanel4.Margin = new System.Windows.Forms.Padding(0);
-            this.ucPanel4.Name = "ucPanel4";
-            this.ucPanel4.Size = new System.Drawing.Size(165, 142);
-            this.ucPanel4.TabIndex = 1;
+            this.ucPanelCenter2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ucPanelCenter2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ucPanelCenter2.Location = new System.Drawing.Point(0, 130);
+            this.ucPanelCenter2.Margin = new System.Windows.Forms.Padding(0);
+            this.ucPanelCenter2.Name = "ucPanelCenter2";
+            this.ucPanelCenter2.Size = new System.Drawing.Size(190, 131);
+            this.ucPanelCenter2.TabIndex = 1;
             // 
-            // ucPanel5
+            // ucPanelRight
             // 
-            this.ucPanel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.ucPanel5.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.ucPanel5.Location = new System.Drawing.Point(165, 0);
-            this.ucPanel5.Margin = new System.Windows.Forms.Padding(0);
-            this.ucPanel5.Name = "ucPanel5";
-            this.tableLayoutPanel2.SetRowSpan(this.ucPanel5, 2);
-            this.ucPanel5.Size = new System.Drawing.Size(166, 284);
-            this.ucPanel5.TabIndex = 2;
+            this.ucPanelRight.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ucPanelRight.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ucPanelRight.Location = new System.Drawing.Point(190, 0);
+            this.ucPanelRight.Margin = new System.Windows.Forms.Padding(0);
+            this.ucPanelRight.Name = "ucPanelRight";
+            this.tableLayoutPanel2.SetRowSpan(this.ucPanelRight, 2);
+            this.ucPanelRight.Size = new System.Drawing.Size(190, 261);
+            this.ucPanelRight.TabIndex = 2;
             // 
-            // ucPanel2
+            // ucPanelTop
             // 
-            this.ucPanel2.BackColor = System.Drawing.Color.Aqua;
-            this.ucPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.ucPanel2.Location = new System.Drawing.Point(0, 0);
-            this.ucPanel2.Margin = new System.Windows.Forms.Padding(0);
-            this.ucPanel2.Name = "ucPanel2";
-            this.ucPanel2.Size = new System.Drawing.Size(337, 105);
-            this.ucPanel2.TabIndex = 1;
+            this.ucPanelTop.BackColor = System.Drawing.Color.Aqua;
+            this.ucPanelTop.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ucPanelTop.Location = new System.Drawing.Point(0, 0);
+            this.ucPanelTop.Margin = new System.Windows.Forms.Padding(0);
+            this.ucPanelTop.Name = "ucPanelTop";
+            this.ucPanelTop.Size = new System.Drawing.Size(386, 96);
+            this.ucPanelTop.TabIndex = 1;
+            // 
+            // lboxLog
+            // 
+            this.lboxLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lboxLog.FormattingEnabled = true;
+            this.lboxLog.ItemHeight = 12;
+            this.lboxLog.Location = new System.Drawing.Point(0, 363);
+            this.lboxLog.Margin = new System.Windows.Forms.Padding(0);
+            this.lboxLog.Name = "lboxLog";
+            this.lboxLog.Size = new System.Drawing.Size(386, 81);
+            this.lboxLog.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(508, 395);
+            this.ClientSize = new System.Drawing.Size(508, 444);
             this.Controls.Add(this.splitContainer1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
@@ -168,13 +182,14 @@
         #endregion
 
         private System.Windows.Forms.SplitContainer splitContainer1;
-        private ucPanel ucPanel1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
-        private ucPanel ucPanel3;
-        private ucPanel ucPanel4;
-        private ucPanel ucPanel5;
-        private ucPanel ucPanel2;
+        private ucPanel ucPanelCenter1;
+        private ucPanel ucPanelCenter2;
+        private ucPanel ucPanelRight;
+        private ucPanel ucPanelTop;
+        private ucColorMenu ucCMenu;
+        private System.Windows.Forms.ListBox lboxLog;
 
 
 
--- DoriLayout/DoriLayout/Form1.cs
+++ DoriLayout/DoriLayout/Form1.cs
@@ -16,5 +16,54 @@ namespace DoriLayout
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ucCMenu.eColorAction += ucCMenu_eColorAction;
+
+            ucPanelTop.eLabelDoubleClickHandler += ucPanel_eLabelDoubleClickHandler;
+            ucPanelCenter1.eLabelDoubleClickHandler += ucPanel_eLabelDoubleClickHandler;
+            ucPanelCenter2.eLabelDoubleClickHandler += ucPanel_eLabelDoubleClickHandler;
+            ucPanelRight.eLabelDoubleClickHandler += ucPanel_eLabelDoubleClickHandler;
+        }
+
+        void ucPanel_eLabelDoubleClickHandler(object sender, EventArgs e)
+        {
+            string strResult = ucCMenu.fButtonColorChange((ucPanel)sender);
+
+            lboxLog.Items.Add(strResult);
+        }
+
+        void ucCMenu_eColorAction(Button arg1, Color arg2)
+        {
+            //Button oBtn = arg1 as Button;
+
+            string strPanelName = string.Empty;
+
+            switch (arg1.Name)
+            {
+                case "btn1":
+                    ucPanelTop.BackColor = arg2;
+                    strPanelName = "Panel Top";
+                    break;
+                case "btn2":
+                    ucPanelCenter1.BackColor = arg2;
+                    strPanelName = "Panel Center1";
+                    break;
+                case "btn3":
+                    ucPanelCenter2.BackColor = arg2;
+                    strPanelName = "Panel Center2";
+                    break;
+                case "btn4":
+                    ucPanelRight.BackColor = arg2;
+                    strPanelName = "Panel Right";
+                    break;
+            }
+
+            string strResult = string.Format("선택 : {0}, {1}의 색상을 {2}로 변경", arg1.Name, strPanelName, arg2.ToString());
+            lboxLog.Items.Add(strResult);
+
+        }
+
     }
 }
