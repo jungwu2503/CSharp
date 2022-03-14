6330c1c652b08e5b8350fc095fd9849d2eaabd4a
술든라이언 <85924877+jungwu2503@users.noreply.github.com>
2022-03-14 15:50:42 +0900

    Add files via upload

---
+++ XDrawer2/XDrawer2/FigureItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XDrawer2
+{
+    class FigureItem : ListViewItem
+    {
+        Figure figure;
+        public FigureItem(String s, Figure figure)
+            : base(s)
+        {
+            this.figure = figure;
+        }
+        public Figure getFigure() {
+            return figure;
+        }
+    }
+}
---
+++ XDrawer2/XDrawer2/FigureTreeNode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XDrawer2
+{
+    public class FigureTreeNode : TreeNode
+    {
+        Figure figure;
+        public FigureTreeNode(String name, Figure figure)
+            : base(name)
+        {
+            this.figure = figure;
+        }
+        public Figure getFigure()
+        {
+            return figure;
+        }
+    }
+}
